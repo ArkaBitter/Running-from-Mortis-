6b0828704d923bc840d0283bbeeb54f189be4240
chocVictory <124659122+ArkaBitter@users.noreply.github.com>
2026-03-02 14:37:43 +0800

    Update white moon log message for character transformation

--- script.js
+++ script.js
@@ -3199,7 +3199,7 @@ class GameScene extends Phaser.Scene {
         this.switchBackgroundMusic('imprisonedII');
         
         // 添加事件到事件日志
-        this.addEvent('已经集齐所有道具，丰川祥子进入了白月光形态！');
+        this.addEvent('集齐了神秘的玩偶、芭蕾舞鞋、素世的香水、巧克力奶、天文望远镜，丰川祥子进入了白月光形态！');
         
         // 点亮白月光形态成就
         const achievements = JSON.parse(localStorage.getItem('gameAchievements') || '{}');
